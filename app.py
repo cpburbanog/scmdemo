f4386b5129f42489036df304d2c35d8897e6246b
cpburbanog <cuentasrbai@gmail.com>
2024-12-16 02:19:03 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "¡Hola! La aplicación está funcionando correctamente."
+    return "¡Hola! La aplicación Flask está funcionando."
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Usar el puerto que Render asigna
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+
